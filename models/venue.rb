1380496872980c95cf775a5b77147757850ece2c
Leighroy <leigh@dice.fm>
2016-03-08 13:25:04 +0000

    2 different ways of doing venue class

--- models/venue.rb
+++ models/venue.rb
@@ -1,1 +1,40 @@
 # init venue file
+
+class Venue
+
+  include MongoMapper::Document
+
+  key :name, String, :required => true
+  key :address_line_1, String, :required => true
+  key :address_line_2, String
+  key :address_line_3, String
+  key :city, String, :required => true
+  key :county, String
+  key :country, String, :required => true
+  key :postcode, String, :required => true
+end
+
+class Venue
+
+  attr_accessor :_id,
+                :name,
+                :address_line_1,
+                :address_line_2,
+                :address_line_3,
+                :city,
+                :county,
+                :country,
+                :postcode
+
+  def initialize(params = {})
+    @_id              = "new object id"
+    @name             = params[:name]
+    @address_line_1   = params[:address_line_1]
+    @address_line_2   = params[:address_line_2] || ""
+    @address_line_3   = params[:address_line_3] || ""
+    @city             = params[:city]
+    @county           = params[:county] || ""
+    @country          = params[:country]
+    @postcode         = params[:postcode]
+  end
+end
