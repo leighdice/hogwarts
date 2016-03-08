836d1eb26999b62a71829bc38936bcde873ce28f
Leighroy <leigh@dice.fm>
2016-03-08 23:09:12 +0000

    required and default fields for venue

--- models/venue.rb
+++ models/venue.rb
@@ -2,12 +2,14 @@ class Venue
 
   include Mongoid::Document
 
-  field :name, type: String
-  field :address_line_1, type: String
-  field :address_line_2, type: String
-  field :address_line_3, type: String
-  field :city, type: String
-  field :county, type: String
-  field :country, type: String
-  field :postcode, type: String
+  field :name,            type: String
+  field :address_line_1,  type: String
+  field :address_line_2,  type: String, default: ->{ "" }
+  field :address_line_3,  type: String, default: ->{ "" }
+  field :city,            type: String
+  field :county,          type: String, default: ->{ "" }
+  field :country,         type: String
+  field :postcode,        type: String
+
+  validates_presence_of :name, :address_line_1, :city, :country, :postcode
 end
