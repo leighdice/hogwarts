cba4348a3aa1b3be7c4d2c0eb7d4a81433880fc9
Leighroy <leigh@dice.fm>
2016-03-10 18:37:37 +0000

    working nicely now, lets concentrate on one type of address

--- test_data/import-test.rb
+++ test_data/import-test.rb
@@ -24,14 +24,27 @@ def generate_venue(v, a, c, id)
 
   begin
   
-    address_line_1 = a.match(/^(.+),/)[1]
-    address_line_2 = a.match(/^(.+),/)[2] || ""
-    address_line_3 = a.match(/^(.+),/)[3] || ""
-    
-    without_address = a.gsub(address_line_1, "")
-    puts without_address
-    postcode = without_address.match(/,[\s]\S+[\s](.+)$/)[1]
+    address_line_1 = a.match(/^[^,]+/)[0]
+    address_line_2 = a.match(/,\s(.+),/)[1] || ""
+    address_line_3 = a.match(/,\s(.+),/)[2] || ""
+
+    postcode_and_city = a.gsub(address_line_1, "")
+    postcode_and_city = postcode_and_city.gsub(address_line_2, "")
+    postcode_and_city = postcode_and_city.gsub(address_line_3, "")
+
+    pm = postcode_and_city.scan(/\s(\w+)/).to_a
+
+    if pm.count == 3
+      city = pm[0][0]
+      postcode = pm[1][0] + " " + pm[2][0]
+    else
+      @exceptions.push(id)
+      return nil
+    end
+      
+
   rescue Exception => e
+    #puts e
     @exceptions.push(id)
     return nil
   end
@@ -41,7 +54,7 @@ def generate_venue(v, a, c, id)
     address_line_1: address_line_1,
     address_line_2: address_line_2,
     address_line_3: address_line_3,
-    city: c,
+    city: city,
     country: "United Kingdom",
     postcode: postcode
   }
@@ -64,7 +77,7 @@ events_dump.each do |e|
     cities = e["cities"].first
     city_name = cities["name"]
 
-    venue = generate_venue(e["venue"], e["address"], city_name, e["_id"])
+    venue = generate_venue(e["venue"], e["address"], city_name, e["id"])
     venue_array.push(venue) unless venue.nil?
   end
 end
