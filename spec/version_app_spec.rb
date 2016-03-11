f8496569cd5f0bb97d81de9f67861e5e34a10e8d
Leighroy <leigh@dice.fm>
2016-03-11 17:32:02 +0000

    Add duration in header test for /version

--- spec/version_app_spec.rb
+++ spec/version_app_spec.rb
@@ -7,4 +7,9 @@ describe "Version" do
     res = JSON.parse(last_response.body)
     expect(res["version"]).to eql("0.0.1")
   end
+
+  it "should respond with duration in header" do
+    get '/version'
+    expect(last_response.header["x-duration"].to_s).to match(/(\d)$/)
+  end
 end
