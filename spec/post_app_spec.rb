15c5c7de7d97731afdbb15d54b272acf07ff6ed4
Leighroy <leigh@dice.fm>
2016-03-11 18:41:31 +0000

    much cleaner way for posts

--- spec/post_app_spec.rb
+++ spec/post_app_spec.rb
@@ -6,29 +6,17 @@ describe "Post Requests" do
 
     describe "Post empty params" do
 
-      base_empty_json = {
-        "address_line_1" => "Richmond Building",
-        "address_line_2" => "University of Bristol Students' Union",
-        "address_line_3" => "105 Queens Rd",
-        "city" => "Bristol",
-        "country" => "United Kingdom",
-        "county" => "",
-        "name" => "Anson Rooms",
-        "postcode" => "BS8 1LN"}
-
       describe "Post empty name" do
 
         it "should respond with status 400" do
-          wo_name = base_empty_json
-          wo_name["name"] = ""
+          wo_name = json_with_empty_param("name")
           post('/venues/new', wo_name.to_json, { "CONTENT_TYPE" => "application/json" })
 
           expect(last_response.status).to eql(400)
         end
 
         it "should respond with correct error body" do
-          wo_name = base_empty_json
-          wo_name["name"] = ""
+          wo_name = json_with_empty_param("name")
           post('/venues/new', wo_name.to_json, { "CONTENT_TYPE" => "application/json" })
           expect(last_response.body).to eql({ :error => "Validation Error",
                                               :code  => 400,
@@ -39,16 +27,14 @@ describe "Post Requests" do
 
       describe "Post empty address_line_1" do
         it "should respond with status 400" do
-          wo_a_l_1 = base_empty_json
-          wo_a_l_1["address_line_1"] = ""
+          wo_a_l_1 = json_with_empty_param("address_line_1")
           post('/venues/new', wo_a_l_1.to_json, { "CONTENT_TYPE" => "application/json" })
 
           expect(last_response.status).to eql(400)
         end
 
         it "should respond with correct error body" do
-          wo_a_l_1 = base_empty_json
-          wo_a_l_1["address_line_1"] = ""
+          wo_a_l_1 = json_with_empty_param("address_line_1")
           post('/venues/new', wo_a_l_1.to_json, { "CONTENT_TYPE" => "application/json" })
           expect(last_response.body).to eql({ :error => "Validation Error",
                                               :code  => 400,
@@ -59,36 +45,32 @@ describe "Post Requests" do
 
       describe "Post empty city" do
         it "should respond with status 400" do
-          wo_city = base_empty_json
-          wo_city["city"] = ""
+          wo_city = json_with_empty_param("city")
           post('/venues/new', wo_city.to_json, { "CONTENT_TYPE" => "application/json" })
 
           expect(last_response.status).to eql(400)
         end
 
         it "should respond with correct error body" do
-          wo_city = base_empty_json
-          wo_city["city"] = ""
+          wo_city = json_with_empty_param("city")
           post('/venues/new', wo_city.to_json, { "CONTENT_TYPE" => "application/json" })
           expect(last_response.body).to eql({ :error => "Validation Error",
                                               :code  => 400,
                                               :description => "Bad Request",
-                                              :messages => { "city":["name cannot be empty"]}}.to_json)
+                                              :messages => { "city":["city cannot be empty"]}}.to_json)
         end
       end
 
       describe "Post empty country" do
         it "should respond with status 400" do
-          wo_country = base_empty_json
-          wo_country["country"] = ""
+          wo_country = json_with_empty_param("country")
           post('/venues/new', wo_country.to_json, { "CONTENT_TYPE" => "application/json" })
 
           expect(last_response.status).to eql(400)
         end
 
         it "should respond with correct error body" do
-          wo_country = base_empty_json
-          wo_country["country"] = ""
+          wo_country = json_with_empty_param("country")
           post('/venues/new', wo_country.to_json, { "CONTENT_TYPE" => "application/json" })
           expect(last_response.body).to eql({ :error => "Validation Error",
                                               :code  => 400,
@@ -99,16 +81,14 @@ describe "Post Requests" do
 
       describe "Post empty postcode" do
         it "should respond with status 400" do
-          wo_postcode = base_empty_json
-          wo_postcode["postcode"] = ""
+          wo_postcode = json_with_empty_param("postcode")
           post('/venues/new', wo_postcode.to_json, { "CONTENT_TYPE" => "application/json" })
 
           expect(last_response.status).to eql(400)
         end
 
         it "should respond with correct error body" do
-          wo_postcode = base_empty_json
-          wo_postcode["postcode"] = ""
+          wo_postcode = json_with_empty_param("postcode")
           post('/venues/new', wo_postcode.to_json, { "CONTENT_TYPE" => "application/json" })
           expect(last_response.body).to eql({ :error => "Validation Error",
                                               :code  => 400,
