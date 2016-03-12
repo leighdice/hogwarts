c515837327a398395a506f7c0796316ef86fc3ca
Leighroy <leigh@dice.fm>
2016-03-12 14:45:03 +0000

    post app clean up

--- spec/post_app_spec.rb
+++ spec/post_app_spec.rb
@@ -4,18 +4,16 @@ describe "Post Requests" do
 
   describe "Post - new venue" do
 
-    it "should respond with status 201" do
-      venue = standard_venue_json
-      post('/venues/new', venue.to_json, { "CONTENT_TYPE" => "application/json" })
+    venue = standard_venue_json
+    post('/venues/new', venue.to_json, { "CONTENT_TYPE" => "application/json" })
+    response = last_response
 
-      expect(last_response.status).to eql(201)
+    it "should respond with status 201" do
+      expect(response.status).to eql(201)
     end
 
     it "should respond with duration in body" do
-      venue = standard_venue_json
-      post('/venues/new', venue.to_json, { "CONTENT_TYPE" => "application/json" })
-      jdata = JSON.parse(last_response.body)
-
+      jdata = JSON.parse(response.body)
       expect(jdata["duration"]).to match(/(\d)$/)
     end
   end
