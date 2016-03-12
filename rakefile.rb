48043e309a1ae72e3ed33b98c5e140bd38b2d29d
Leighroy <leigh@dice.fm>
2016-03-12 13:12:41 +0000

    init rakefile

---
+++ rakefile.rb
@@ -0,0 +1,11 @@
+require 'rspec/core/rake_task'
+
+desc "Run the specs."
+task :test => [:import_test_data] do
+  system('bundle exec rspec')
+end
+
+desc "Restore mongo with venue test data"
+task :import_test_data do
+  sh("./scripts/restore-venue-db")
+end
