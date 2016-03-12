d85ff6a7163dafc332cf3d7f3cad7706752c093b
Leighroy <leigh@dice.fm>
2016-03-12 14:53:01 +0000

    Version clean up spec

--- spec/version_app_spec.rb
+++ spec/version_app_spec.rb
@@ -2,14 +2,15 @@ require_relative 'app_spec_helper'
 
 describe "Version" do
 
+  get '/version'
+  response = last_response
+
   it "should respond with version number" do
-    get '/version'
-    res = JSON.parse(last_response.body)
+    res = JSON.parse(response.body)
     expect(res["version"]).to eql("0.0.1")
   end
 
   it "should respond with duration in header" do
-    get '/version'
-    expect(last_response.header["x-duration"].to_s).to match(/(\d)$/)
+    expect(response.header["x-duration"].to_s).to match(/(\d)$/)
   end
 end
