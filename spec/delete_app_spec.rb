5e8ab1ddb747f973f95673ab7bcee33b092c37f3
Leighroy <leigh@dice.fm>
2016-03-12 14:37:19 +0000

    delete by id - also found cleanrer way of not repating

--- spec/delete_app_spec.rb
+++ spec/delete_app_spec.rb
@@ -4,18 +4,30 @@ describe "Delete" do
 
   describe "Delete Valid ID" do
 
+    id = "56e40252133859262d000021"
+    delete "/venues/#{id}"
+    response = last_response
+
+    it "should respond with status 204" do
+      expect(response.status).to eql(204)
+    end
+
+    it "should respond with duration in the header" do
+      expect(response.header["x-duration"].to_s).to match(/(\d)$/)
+    end
   end
 
   describe "Delete Invalid ID" do
 
+    delete '/venues/someInvalidID'
+    response = last_response
+
     it "should respond with status 404" do
-      delete '/venues/123'
-      expect(last_response.status).to eql(404)
+      expect(response.status).to eql(404)
     end
 
     it "should return correct body error" do
-      delete '/venues/123'
-      expect(last_response.body).to eql(not_found_with_id_error("123"))
+      expect(response.body).to eql(not_found_with_id_error("someInvalidID"))
     end
   end
 end
