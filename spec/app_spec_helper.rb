9373068fb93d82a8ec0ea125779b9b51b1acd9bb
Leighroy <leigh@dice.fm>
2016-03-13 17:17:06 +0000

    try setting environment to dev

--- spec/app_spec_helper.rb
+++ spec/app_spec_helper.rb
@@ -2,9 +2,8 @@ require_relative File.join(File.dirname(__FILE__), '../app.rb')
 require_relative File.join(File.dirname(__FILE__), '/support/test_helper.rb')
 require 'rspec'
 require 'rack/test'
-require 'mongoid'
 
-set :environment, :test
+set :environment, :development
 
 
 include Rack::Test::Methods
