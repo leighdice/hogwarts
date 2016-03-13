ba1e257d946cfedf0c4712178a5178d7f4c9bcb8
Leighroy <leigh@dice.fm>
2016-03-13 17:14:05 +0000

    try requiring mongoid

--- spec/app_spec_helper.rb
+++ spec/app_spec_helper.rb
@@ -2,6 +2,7 @@ require_relative File.join(File.dirname(__FILE__), '../app.rb')
 require_relative File.join(File.dirname(__FILE__), '/support/test_helper.rb')
 require 'rspec'
 require 'rack/test'
+require 'mongoid'
 
 set :environment, :test
 
