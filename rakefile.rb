d48bd9c47e2aca04e8070cafc73e3ad858d463b2
Leighroy <leigh@dice.fm>
2016-03-16 15:26:34 +0000

    use rspec rake task

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,11 @@
+require 'rspec/core/rake_task'
+
 desc "Run the specs."
 task :test => [:import_test_data] do
-  system('bundle exec rspec')
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.pattern = 'spec/*_spec.rb'
+  end
+  Rake::Task["spec"].execute
 end
 
 desc "Restore mongo with venue test data"
